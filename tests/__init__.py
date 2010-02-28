89816968c3e640458268e43191e93c35f3671326
Waldemar Kornewald <wkornewald>
2010-02-28 21:14:37 +0100

    removed transaction tests (transactions are an inofficial hack)

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,5 +3,4 @@ from .field_db_conversion import FieldDBConversionTest
 from .field_options import FieldOptionsTest
 from .filter import FilterTest
 from .order import OrderTest
-from .transaction import TxTest
 from .not_return_sets import NonReturnSetsTest
