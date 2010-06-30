f380f3611d4eb344fcdb24cc2991dd98ecd85cd8
Waldemar Kornewald <wkornewald>
2010-06-30 10:20:52 +0200

    added unit test for multi-table inheritance exception

--- tests/backend.py
+++ tests/backend.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.test import TestCase
+from django.db.utils import DatabaseError
 
 class A(models.Model):
     value = models.IntegerField()
+
+class B(A):
+    other = models.IntegerField()
 
 class BackendTest(TestCase):
     def test_model_forms(self):
@@ -12,3 +16,10 @@ class BackendTest(TestCase):
                 model = A
 
         F({'value': '3'}).save()
+
+    def test_multi_table_inheritance(self):
+        B(value=3, other=5).save()
+        self.assertEqual(A.objects.count(), 1)
+        self.assertEqual(A.objects.all()[0].value, 3)
+        self.assertRaises(DatabaseError, B.objects.count)
+        self.assertRaises(DatabaseError, lambda: B.objects.all()[0])
