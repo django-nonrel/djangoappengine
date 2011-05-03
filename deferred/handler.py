9f176acdf46562b0318fdf02226e3f2fa1837a2c
Thomas Wanschik <twanschik>
2011-05-03 16:15:09 +0200

    added loading of all models.py from settings.INSTALLED_APPS to ensure loading of indexes or signal handlers for deferred

--- deferred/handler.py
+++ deferred/handler.py
@@ -1,6 +1,19 @@
 # Initialize Django
 from djangoappengine.main.main import make_profileable
 
+from django.utils.importlib import import_module
+from django.conf import settings
+
+# load all models.py to ensure signal handling installation or index loading
+# of some apps 
+for app in settings.INSTALLED_APPS:
+    try:
+        import_module('%s.models' % (app))
+        import logging
+        logging.debug(app)
+    except ImportError:
+        pass
+
 from google.appengine.ext.deferred.handler import main
 
 main = make_profileable(main)
