a5d9d87f70efb9a7fccda0994906c5c79ea59b3d
refack <devnull@localhost>
2011-10-09 12:36:51 +0200

    Add mapreduce handler

---
+++ mapreduce/handler.py
@@ -0,0 +1,19 @@
+# Initialize Django
+from djangoappengine.main.main import make_profileable
+from django.utils.importlib import import_module
+from django.conf import settings
+
+# load all models.py to ensure signal handling installation or index loading
+# of some apps
+for app in settings.INSTALLED_APPS:
+    try:
+        import_module('%s.models' % app)
+    except ImportError:
+        pass
+
+from google.appengine.ext.mapreduce.main import APP as application, main
+
+_main = make_profileable(main)
+
+if __name__ == '__main__':
+    _main()
