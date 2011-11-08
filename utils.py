dc18955a019276b70db813888939dae8d4564144
Waldemar Kornewald <wkornewald>
2011-11-08 15:18:36 +0100

    fixed manage.py remote

--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ else:
         from .boot import PROJECT_DIR
         appconfig = dev_appserver.LoadAppConfig(PROJECT_DIR, {},
                                                 default_partition='dev')[0]
-        appid = appconfig.application
+        appid = appconfig.application.split('~', 1)[-1]
     except ImportError, e:
         raise Exception('Could not get appid. Is your app.yaml file missing? '
                         'Error was: %s' % e)
