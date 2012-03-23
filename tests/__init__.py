22d9bae4e028880ad501e2dedb881e2ea33121ea
Alex Burgel <aburgel@gmail.com>
2012-03-23 16:30:35 -0400

    remove decimal test import

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,4 @@
 from .backend import BackendTest
-from .decimals import DecimalTest
 from .field_db_conversion import FieldDBConversionTest
 from .field_options import FieldOptionsTest
 from .filter import FilterTest
