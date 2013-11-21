a8762ad28b561beb5a65838e571542f2beae2a90
Alex Burgel <aburgel@gmail.com>
2013-11-21 16:38:48 -0500

    Rename tests to match Django 1.6 conventions

--- djangoappengine/tests/__init__.py
+++ djangoappengine/tests/__init__.py
@@ -1,9 +1,9 @@
-from .backend import BackendTest
-from .field_db_conversion import FieldDBConversionTest
-from .field_options import FieldOptionsTest
-from .filter import FilterTest
-from .keys import KeysTest
-from .mapreduce_input_readers import DjangoModelInputReaderTest, DjangoModelIteratorTest
-from .not_return_sets import NonReturnSetsTest
-from .order import OrderTest
-from .transactions import TransactionTest
+from .test_backend import BackendTest
+from .test_field_db_conversion import FieldDBConversionTest
+from .test_field_options import FieldOptionsTest
+from .test_filter import FilterTest
+from .test_keys import KeysTest
+from .test_mapreduce import DjangoModelInputReaderTest, DjangoModelIteratorTest
+from .test_not_return_sets import NonReturnSetsTest
+from .test_order import OrderTest
+from .test_transactions import TransactionTest
