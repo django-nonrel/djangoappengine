13c882cf9e98e1ac33ada5d4fcffbc7bec2e9158
Alex Burgel <aburgel@gmail.com>
2013-11-28 13:09:42 -0500

    Change setup.py to use distutils

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import find_packages, setup
+from distutils.core import setup
+import os
+
+def get_packages(package):
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 
 DESCRIPTION = 'App Engine backends for Django-nonrel'
@@ -8,27 +14,29 @@ try:
 except:
     pass
 
-
 setup(name='djangoappengine',
       version='1.6.0',
-      packages=find_packages(exclude=['docs']),
-      install_requires=['djangotoolbox'],
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
       author='Waldemar Kornewald',
       author_email='wkornewald@gmail.com',
       url='https://github.com/django-nonrel/djangoappengine',
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
+      packages=get_packages('djangoappengine'),
+      requires=['djangotoolbox (>=1.6.0)'],
       license='3-clause BSD',
-      platforms=['any'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Web Environment',
           'Framework :: Django',
           'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
           'Programming Language :: Python',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Database',
           'Topic :: Software Development :: Libraries :: Application Frameworks',
           'Topic :: Software Development :: Libraries :: Python Modules',
-          'License :: OSI Approved :: BSD License',
-        ],
+      ],
 )
